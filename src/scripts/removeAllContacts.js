c39b58c936bf5dc4a71b9a1743fb881ab4a6c39b
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-06-12 00:37:11 +0300

    some change

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -3,7 +3,7 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const removeAllContacts = async () => {
   try {
-    await fs.writeFile(PATH_DB, [], 'utf8');
+    await fs.writeFile(PATH_DB, JSON.stringify([]), 'utf8');
     console.log('All contacts have been removed.');
   } catch (error) {
     console.error('Error removing contacts:', error);
